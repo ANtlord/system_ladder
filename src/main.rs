dc9701401190b08d1a639074ce6594f5179b1247
ANtlord <antlord92@gmail.com>
2019-05-04 23:51:46 +0800

    Searching processes uses an arbitrary file is implemented.

--- src/main.rs
+++ src/main.rs
@@ -9,30 +9,12 @@ mod process;
 mod utils;
 mod tree;
 
+use std::marker::PhantomData;
 use std::env::args;
 use list::List;
 use user::getpwnam;
 use tree::Node;
 use tree::make_tree;
-//use std::collections;
 
 fn main() {
-    //let user_data = args().skip(1).next().and_then(|x| getpwnam(&x).ok()).expect("Provide user name");
-
-    let mut user_process_vec = vec!();
-    for e in process::get_all_processes() {
-        user_process_vec.push(e);
-    }
-
-    let mut fake_process = Node::new(process::Process{name: "".to_owned(), pid: 0, ppid: 0});
-    make_tree(&mut fake_process, &mut user_process_vec, |x, y| x.pid == y.ppid);
-    debug_assert_eq!(user_process_vec.len(), 0, "{:?}", &user_process_vec);
-    print_tree(&fake_process, 0);
-}
-
-fn print_tree(n: &Node<process::Process>, indent: usize) {
-    println!("{}> {} {} {}", "-".repeat(indent), n.value.name, n.value.pid, n.value.ppid);
-    for e in &n.children {
-        print_tree(&e, indent + 1)
-    }
 }
--- src/process/mod.rs
+++ src/process/mod.rs
@@ -2,6 +2,7 @@ use std::fs;
 use std::io::BufRead;
 use std::io::BufReader;
 use std::os::unix::fs::MetadataExt;
+use std::path;
 
 
 fn is_user_folder(dir: &fs::DirEntry, uid: u32) -> bool {
@@ -42,6 +43,16 @@ impl Process {
         bufreader.lines().filter_map(|x| x.ok()).for_each(|x| fill_process(&mut p, &x));
         p
     }
+
+    pub fn opened_files(&self) -> impl Iterator<Item=path::PathBuf> {
+        let process_file_desctriptors_path: path::PathBuf = [
+            "/proc", &self.pid.to_string(), "fd",
+        ].iter().collect();
+        let file_descriptors_dir_reading = process_file_desctriptors_path.as_path().read_dir().unwrap();
+        let file_descriptors_dir_entries = file_descriptors_dir_reading.filter_map(|x| x.ok());
+        let process_opened_files = file_descriptors_dir_entries.map(|x| x.path()).map(|x| fs::read_link(x));
+        process_opened_files.filter_map(|x| x.ok()).filter(|x| x.is_file())
+    }
 }
 
 pub fn get_processes(uid: u32) -> impl Iterator<Item=Process> {
@@ -61,4 +72,24 @@ pub fn get_all_processes() -> impl Iterator<Item=Process> {
     let proc_dir_entries = fs::read_dir("/proc/").unwrap();
     let process_folders = proc_dir_entries.filter_map(|x| x.ok()).filter(is_process_folder);
     process_folders.map(|x| Process::new(x.path().join("status").to_str().unwrap()))
+}
+
+pub fn print_processes_uses_file(pattern: &str) {
+    for process in get_all_processes() {
+        for f in process.opened_files().filter(|x| is_file_path_desited(x, pattern)).take(1) {
+            match f.to_str() {
+                Some(x) => println!("{} -> {}", process.name, x),
+                None => {
+                    panic!("Can't read link {} of process {}", f.as_path().display(), process.name)
+                },
+            };
+        }
+    }
+}
+
+fn is_file_path_desited(filepath: &path::PathBuf, pattern: &str) -> bool {
+    match filepath.to_str() {
+        None => false,
+        Some(x) => x.contains(pattern),
+    }
 }
