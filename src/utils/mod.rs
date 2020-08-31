e32b3b90f8e93d013148896697f9b6202ee86ea7
ANtlord <antlord92@gmail.com>
2020-09-01 00:21:25 +0300

    Merge sort is implemented.

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,6 +1,9 @@
 pub mod vector;
 
 use std::mem;
+use std::ptr;
+use std::fmt;
+
 
 fn gcd(mut left: u64, mut right: u64) -> u64 {
     while right != 0 {
@@ -10,7 +13,7 @@ fn gcd(mut left: u64, mut right: u64) -> u64 {
     left
 }
 
-fn insertionsort<T, F: Fn(&T, &T) -> bool>(myslice: &mut [T], less: F) {
+pub fn insertionsort<T, F: Fn(&T, &T) -> bool>(myslice: &mut [T], less: F) {
     let len = myslice.len();
     for i in 0 .. len {
         for j in (1 ..= i).rev() {
@@ -19,7 +22,6 @@ fn insertionsort<T, F: Fn(&T, &T) -> bool>(myslice: &mut [T], less: F) {
             }
         }
     }
-
 }
 
 fn actual_quicksort<T, F: Fn(&T, &T) -> bool>(myslice: &mut [T], less: &F) {
@@ -28,7 +30,7 @@ fn actual_quicksort<T, F: Fn(&T, &T) -> bool>(myslice: &mut [T], less: &F) {
         return;
     }
 
-    if len < 20 {
+    if len < 5 {
         return insertionsort(myslice, less);
     }
 
@@ -51,6 +53,74 @@ fn actual_quicksort<T, F: Fn(&T, &T) -> bool>(myslice: &mut [T], less: &F) {
     // myslice.swap(i, last);
     actual_quicksort(&mut myslice[0 .. lt], less);
     actual_quicksort(&mut myslice[gt + 1 .. len], less);
+}
+
+pub fn _merge<T, F: Fn(&T, &T) -> bool>(myslice: &mut [T], less: &F) {
+    let len = myslice.len();
+    let mid = len / 2;
+    let mut buf = Vec::with_capacity(mid);
+    let mut myslice_ptr = myslice.as_mut_ptr();
+    let mut bufptr = buf.as_mut_ptr();
+    unsafe {
+        ptr::copy_nonoverlapping(myslice_ptr, bufptr, mid);
+        buf.set_len(mid);
+    }
+
+    let mut left_cursor = 0;
+    let mut right_cursor = mid;
+    let mut i = 0;
+    debug_assert!(mid == buf.len());
+    while left_cursor < mid && right_cursor < myslice.len() {
+        let left = &buf[left_cursor];
+        let right = &myslice[right_cursor];
+        if less(left, right) {
+            unsafe {
+                ptr::copy_nonoverlapping(bufptr.add(left_cursor), myslice_ptr.add(i), 1);
+            }
+
+            left_cursor += 1;
+            i += 1;
+            continue;
+        }
+
+        myslice.swap(i, right_cursor);
+        right_cursor += 1;
+        i += 1;
+    }
+
+    while left_cursor < mid {
+        unsafe {
+            ptr::copy_nonoverlapping(bufptr.add(left_cursor), myslice_ptr.add(i), 1);
+        }
+
+        i += 1;
+        left_cursor += 1;
+    }
+
+    while right_cursor < myslice.len() {
+        myslice.swap(i, right_cursor);
+        i += 1;
+        right_cursor += 1;
+    }
+    unsafe {
+        let v: Vec<u8> = mem::transmute(buf); // TODO: to prevent drops of T. Find a better way. Look closer to merge_sort in standard library or Rustonomicon
+    }
+}
+
+pub fn _mergesort<T, F: Fn(&T, &T) -> bool>(myslice: &mut [T], less: &F) {
+    let len = myslice.len();
+    let mid = len / 2;
+    if len < 2 {
+        return;
+    }
+
+    _mergesort(&mut myslice[..mid], less);
+    _mergesort(&mut myslice[mid..], less);
+    _merge(myslice, less);
+}
+
+pub fn mergesort<T, F: Fn(&T, &T) -> bool>(myslice: &mut [T], less: F) {
+    _mergesort(myslice, &less);
 }
 
 // TODO: shuffling to guarantee performance.
@@ -123,6 +193,59 @@ mod tests {
             let mut v = vec![3,2,1];
             insertionsort(&mut v, |a, b| a < b);
             assert_eq!(v, vec![1,2,3]);
+        }
+    }
+
+    mod merge {
+        use super::*;
+
+        #[test]
+        fn odd_elements() {
+            let mut v = vec![3,2,1];
+            mergesort(&mut v, |a, b| a < b);
+            assert_eq!(v, vec![1,2,3]);
+        }
+
+        #[test]
+        fn even_elements() {
+            let mut v = vec![4,3,2,1];
+            mergesort(&mut v, |x, y| x < y);
+            assert_eq!(v, vec![1,2,3,4]);
+        }
+
+        #[test]
+        fn equal_elements() {
+            let mut v = vec![4, 4, 2, 1, 0, 8, 7, 7];
+            mergesort(&mut v, |x, y| x < y);
+            assert_eq!(v, vec![0, 1, 2 ,4, 4, 7, 7, 8]);
+        }
+
+        struct Droplet(i32, *mut u32);
+        impl Drop for Droplet {
+            fn drop(&mut self) {
+                unsafe {
+                    (*self.1) += 1;
+                }
+            }
+        }
+
+        #[test]
+        fn drop() {
+            let mut drop_count = 0u32;
+            let mut v = vec![
+                Droplet(20, &mut drop_count as *mut _),
+                Droplet(80, &mut drop_count as *mut _),
+                Droplet(30, &mut drop_count as *mut _),
+                Droplet(40, &mut drop_count as *mut _),
+                Droplet(10, &mut drop_count as *mut _),
+                Droplet(60, &mut drop_count as *mut _),
+                Droplet(90, &mut drop_count as *mut _),
+                Droplet(70, &mut drop_count as *mut _),
+                Droplet(50, &mut drop_count as *mut _),
+            ];
+
+            mergesort(&mut v, |x, y| x.0 < y.0);
+            assert_eq!(drop_count, 0);
         }
     }
 }
